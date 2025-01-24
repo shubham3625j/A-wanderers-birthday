2e30db2fc43522e8ce6b57db5ae13feb5d38450f
Shubham kumar verma <162824296+shubham3625j@users.noreply.github.com>
2025-01-24 23:39:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 27, 2024',
+  birthdate: 'Jan 25, 2025',
   name: 'PRANJALI'
 };
 
